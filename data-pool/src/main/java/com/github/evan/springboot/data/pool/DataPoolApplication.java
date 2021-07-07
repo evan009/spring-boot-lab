b0f12b30831ae109b9f6bd9112089d4c2ac2648b
liuawei <liuawei@lerke.cn>
2021-07-07 18:34:06 +0800

    数据源相关

---
+++ data-pool/src/main/java/com/github/evan/springboot/data/pool/DataPoolApplication.java
@@ -0,0 +1,13 @@
+package com.github.evan.springboot.data.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPoolApplication.class, args);
+    }
+
+}
---
+++ data-pool/src/test/java/com/github/evan/springboot/data/pool/DataPoolApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.evan.springboot.data.pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPoolApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
